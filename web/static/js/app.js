f2519c31d05e580db922134d75ecb2ca54b638f5
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-06-25 18:06:19 -0400

    Add React and jQuery

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,6 +12,8 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import React from "react"
+import ReactDOM from "react-dom"
 
 // Import local files
 //
