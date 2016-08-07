82695893223891e6d625d6d3a540ebd16d04f5b5
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-08-06 23:20:10 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@
 
   def project do
     [app: :battleship,
-     version: "0.1.4",
+     version: "0.1.5",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
