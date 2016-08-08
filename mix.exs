1f2c97bdaac6053bf5a8c75d099b171774a70674
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-08-07 22:33:07 -0400

    Fall back to old versioning strategy

--- mix.exs
+++ mix.exs
@@ -2,10 +2,8 @@
   use Mix.Project
 
   def project do
-    {<< git_sha::bytes-size(7) >> <> _rest, _exit_code} = System.cmd("git", ["rev-parse", "HEAD"])
-
     [app: :battleship,
-     version: "0.1.7-#{git_sha}",
+     version: "0.1.7",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
--- release.sh
+++ release.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-git pull # Get latest code (dependencies: git, already cloned repo)
+read -p "What is the version? " -n 5 -r
+echo
+if [[ $REPLY ]]
+then
+  git pull # Get latest code (dependencies: git, already cloned repo)
 
-npm install # Get js dependencies (dev for bruch and prod for assets) (dependencies: nodejs, npm)
-brunch build --production # Compile static assets for prod (dependencies: brunch(globally installed))
-MIX_ENV=prod mix phoenix.digest # Add digest to compiled assets
+  npm install # Get js dependencies (dev for bruch and prod for assets) (dependencies: nodejs, npm)
+  brunch build --production # Compile static assets for prod (dependencies: brunch(globally installed))
+  MIX_ENV=prod mix phoenix.digest # Add digest to compiled assets
 
-mix deps.get # Get dependencies (dependencies: erlang, elixir)
-MIX_ENV=prod mix do compile, release # Compile and release
+  mix deps.get --only prod # Get prod dependencies (dependencies: erlang, elixir)
+  MIX_ENV=prod mix do compile, release # Compile and release
 
-version=$(mix run -e 'IO.puts Mix.Project.config[:version]')
-echo "Upgrading to $version..."
-
-mkdir "/var/www/battleship/releases/$version"
-cp "rel/battleship/releases/$version/battleship.tar.gz" "/var/www/battleship/releases/$version/"
-/var/www/battleship/bin/battleship upgrade "$version"
+  mkdir "/var/www/battleship/releases/$REPLY"
+  cp "rel/battleship/releases/$REPLY/battleship.tar.gz" "/var/www/battleship/releases/$REPLY/"
+  /var/www/battleship/bin/battleship upgrade "$REPLY"
+fi
