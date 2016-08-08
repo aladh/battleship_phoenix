7508cb642d790dbc79f27dada15d1fad2409ed24
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-08-07 22:50:47 -0400

    Reorder as npm install depends on hex packages

--- release.sh
+++ release.sh
@@ -6,11 +6,12 @@ if [[ $REPLY ]]
 then
   git pull # Get latest code (dependencies: git, already cloned repo)
 
+  mix deps.get --only prod # Get prod dependencies (dependencies: erlang, elixir)
+
   npm install # Get js dependencies (dev for bruch and prod for assets) (dependencies: nodejs, npm)
   brunch build --production # Compile static assets for prod (dependencies: brunch(globally installed))
   MIX_ENV=prod mix phoenix.digest # Add digest to compiled assets
 
-  mix deps.get --only prod # Get prod dependencies (dependencies: erlang, elixir)
   MIX_ENV=prod mix do compile, release # Compile and release
 
   mkdir "/var/www/battleship/releases/$REPLY"
