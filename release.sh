72d2720c2eced94d993c1bf7817e1d0470319faf
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-08-07 21:20:26 -0400

    Only start server when SERVER env variable is set, to allow getting the version

--- release.sh
+++ release.sh
@@ -6,10 +6,10 @@ npm install # Get js dependencies (dev for bruch and prod for assets) (dependenc
 brunch build --production # Compile static assets for prod (dependencies: brunch(globally installed))
 MIX_ENV=prod mix phoenix.digest # Add digest to compiled assets
 
-mix deps.get # Get dependencies (dependencies: erlang, elixir)
-MIX_ENV=prod mix do compile, release,  # Compile and release
+mix deps.get --only prod # Get prod dependencies (dependencies: erlang, elixir)
+SERVER=1 MIX_ENV=prod mix do compile, release # Compile and release
 
-version=$(mix run -e 'IO.puts Mix.Project.config[:version]')
+version=$(MIX_ENV=prod mix run -e 'IO.puts Mix.Project.config[:version]')
 echo "Upgrading to $version..."
 
 mkdir "/var/www/battleship/releases/$version"
