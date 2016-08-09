fe5643fb13e40350e00782fa002d716c2a6dead6
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-08-08 23:37:27 -0400

    Block bag guesses when the square is at the beginning or end of row

--- lib/battleship/targeting/proximity.ex
+++ lib/battleship/targeting/proximity.ex
@@ -10,32 +10,45 @@ defmodule Battleship.Targeting.Proximity do
     if guess, do: guess, else: ship_guess(rest, board)
   end
 
-  defp ship_guess([], _board) do
-    nil
-  end
+  defp ship_guess([], _board), do: nil
 
   defp square_guess(square, board) do
+    index = square.index
     cond do
-      index_guess(square.index - 1, board) ->
+      guess = index_guess(index, index - 1, board) ->
         IO.puts "Guessing left"
-        index_guess(square.index - 1, board)
-      index_guess(square.index + 1, board) ->
+        guess
+      guess = index_guess(index, index + 1, board) ->
         IO.puts "Guessing right"
-        index_guess(square.index + 1, board)
-      index_guess(square.index - Battleship.constants.row_length, board) ->
+        guess
+      guess = index_guess(index, index - Battleship.constants.row_length, board) ->
         IO.puts "Guessing one row above"
-        index_guess(square.index - Battleship.constants.row_length, board)
-      index_guess(square.index + Battleship.constants.row_length, board) ->
+        guess
+      guess = index_guess(index, index + Battleship.constants.row_length, board) ->
         IO.puts "Guessing one row below"
-        index_guess(square.index + Battleship.constants.row_length, board)
+        guess
       true ->
         nil
     end
   end
 
-  defp index_guess(index, board) do
-    if valid_guess?(index, board), do: index
+  defp index_guess(square_index, guess_index, board) when guess_index == square_index - 1 do
+    if first_in_row(square_index), do: IO.puts "blocking left guess (#{guess_index}) for square at #{square_index}"
+    if valid_guess?(guess_index, board) && !first_in_row(square_index), do: guess_index
   end
+
+  defp index_guess(square_index, guess_index, board) when guess_index == square_index + 1 do
+    if last_in_row(square_index), do: IO.puts "blocking right guess (#{guess_index}) for square at #{square_index}"
+    if valid_guess?(guess_index, board) && !last_in_row(square_index), do: guess_index
+  end
+
+  defp index_guess(_square_index, guess_index, board) do
+    if valid_guess?(guess_index, board), do: guess_index
+  end
+
+  defp first_in_row(index), do: rem(index, 8) == 0
+
+  defp last_in_row(index), do: rem(index + 1, 8) == 0
 
   defp valid_guess?(index, board) do
     revealed = Enum.at(board.squares, index)
