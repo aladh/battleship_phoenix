87e00f06a82c5fe5802e44b389ac4c0398b5d0da
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-08-09 00:06:37 -0400

    Refactor

--- lib/battleship/targeting/proximity/square.ex
+++ lib/battleship/targeting/proximity/square.ex
@@ -2,20 +2,31 @@ defmodule Battleship.Targeting.Proximity.Square do
   def guess(square, board) do
     index = square.index
     cond do
-      guess_index = Battleship.Targeting.Proximity.Index.guess(index, index - 1, board) ->
-        IO.puts "Guessing left"
-        guess_index
-      guess_index = Battleship.Targeting.Proximity.Index.guess(index, index + 1, board) ->
-        IO.puts "Guessing right"
-        guess_index
-      guess_index = Battleship.Targeting.Proximity.Index.guess(index, index - Battleship.constants.row_length, board) ->
-        IO.puts "Guessing one row above"
-        guess_index
-      guess_index = Battleship.Targeting.Proximity.Index.guess(index, index + Battleship.constants.row_length, board) ->
-        IO.puts "Guessing one row below"
-        guess_index
-      true ->
-        nil
+      guess_index = left_guess(index, board) -> guess_index
+      guess_index = right_guess(index, board) -> guess_index
+      guess_index = up_guess(index, board) -> guess_index
+      guess_index = down_guess(index, board) -> guess_index
+      true -> nil
     end
+  end
+
+  defp left_guess(square_index, board) do
+    IO.puts "Guessing left"
+    Battleship.Targeting.Proximity.Index.guess(square_index, square_index - 1, board)
+  end
+
+  defp right_guess(square_index, board) do
+    IO.puts "Guessing right"
+    Battleship.Targeting.Proximity.Index.guess(square_index, square_index + 1, board)
+  end
+
+  defp up_guess(square_index, board) do
+    IO.puts "Guessing one row above"
+    Battleship.Targeting.Proximity.Index.guess(square_index, square_index - Battleship.constants.row_length, board)
+  end
+
+  defp down_guess(square_index, board) do
+    IO.puts "Guessing one row below"
+    Battleship.Targeting.Proximity.Index.guess(square_index, square_index + Battleship.constants.row_length, board)
   end
 end
