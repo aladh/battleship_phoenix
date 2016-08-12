f6a8d61dc9b06f7f7217bbd18a4c413133329824
Alishan Ladhani <Alishan.Ladhani@gmail.com>
2016-08-11 22:34:41 -0400

    Use Square directly as there is only one square

--- lib/battleship/targeting/proximity.ex
+++ lib/battleship/targeting/proximity.ex
@@ -1,7 +1,7 @@
 defmodule Battleship.Targeting.Proximity do
   def guess([[ship_square] | _rest], board) do
     IO.puts "Proximity: single"
-    Battleship.Targeting.Proximity.Ship.guess([ship_square], board)
+    Battleship.Targeting.Proximity.Square.guess(ship_square, board)
   end
 
   def guess([ship_squares | _rest], board) do
